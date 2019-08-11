727cc5386500d9eb6b888061d4d89fb06df0ca9e
Todly <furrykunstler@gmail.com>
2019-08-11 15:50:45 +0300

    Added pause menu

---
+++ Platformer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private Shooting shooter;
+    private bool isPaused;
+
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if (shooter == null)
+        {
+            shooter = GameObject.FindObjectOfType<Shooting>();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        shooter.enabled = true;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        shooter.enabled = false;
+        isPaused = true;
+    }
+
+}
